b62208b322338b2d07da8fdaf54dc191137694db
zahra arefnia <zahra.arfnia@gmail.com>
2026-02-14 20:42:21 +0330

    edit important page

--- to-do/src/app/important/page.tsx
+++ to-do/src/app/important/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import{TasksPage} from "@/components/taskpage/taskpage"
+import{TasksPage} from "@/src/components/taskpage/taskpage"
 export default function ImportantPage() {
 return(
   <TasksPage filterType="important" />
